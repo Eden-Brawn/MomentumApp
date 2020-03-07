24dd16b6bbe5f0d51d323d65ca1a0f03b87e28b7
Eden Brawn <edenbrawn@gmail.com>
2020-03-07 16:29:42 +0000

    commit2

---
+++ index.js
@@ -0,0 +1,9 @@
+window.onload = function (){
+
+}
+
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let btn = document.getElementById("btn");
+let list = document.getElementById("list");
+let btnclr = document.getElementById("btnclr");
